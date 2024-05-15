84ed9b0b52f6b1ef1ca91edb23ecf307f9d90822
Treasure's Dell <peacemary259@gmail.com>
2024-05-15 17:33:11 +0100

    initialCommit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.querySelector('.btn').addEventListener('click', clickBtn)
+const colorBox = document.querySelector('.color')
+function clickBtn(){
+ colorBox.style.backgroundColor = 'green'
+}
